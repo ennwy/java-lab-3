587684f21cd8dd50b7711b4d3187ed6be7a36422
Mykyta Shchedrin <m.shchedrin@overdone.it>
2024-12-20 19:41:14 +0200

    Added setters to Item class, simplified if logic

--- lab3/GildedRose.java
+++ lab3/GildedRose.java
@@ -7,48 +7,41 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item:items) {
-            if (item.name.equals("Aged Brie")
-                    || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1; 
-                        }
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+            if (item.name.equals("Aged Brie")) {
+                item.increaseQualityByOne();
+            }
+            
+            else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.increaseQualityByOne();
+        
+                if (item.sellIn < 11) {
+                    item.increaseQualityByOne();
                 }
-            } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+        
+                if (item.sellIn < 6) {
+                    item.increaseQualityByOne();
                 }
             }
 
+            else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.decreaseQualityByOne();
+            }
+
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
+                item.decreaseSellInByOne();
             }
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.decreaseQualityByOne();
                         }
                     } else {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.increaseQualityByOne();
                 }
             }
         }
--- lab3/Item.java
+++ lab3/Item.java
@@ -12,6 +12,24 @@ public class Item {
         this.quality = quality;
     }
 
+    public int increaseQualityByOne() {
+        if(quality < 50) {
+            return quality++;
+        }
+        return quality;
+    }
+
+    public int decreaseQualityByOne() {
+        if(quality > 0) {
+            return quality--;
+        }
+        return quality;
+    }
+
+    public int decreaseSellInByOne() {
+        return sellIn--;
+    }
+    
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
